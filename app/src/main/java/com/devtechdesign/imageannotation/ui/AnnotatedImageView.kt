be23c70d3b01ec210e31df84f51fa20c4d016d99
Matt Roberts <>
2020-09-09 21:10:41 -0700

    add the ability to add multiple goemetryviews

--- app/src/main/java/com/devtechdesign/imageannotation/ui/AnnotatedImageView.kt
+++ app/src/main/java/com/devtechdesign/imageannotation/ui/AnnotatedImageView.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.children
 import com.devtechdesign.imageannotation.R
+import kotlinx.android.synthetic.main.annotated_image_view.view.*
 
 class AnnotatedImageView : FrameLayout {
 
@@ -36,5 +40,29 @@ class AnnotatedImageView : FrameLayout {
 
     fun setImageDrawable(drawable: Drawable) {
         findViewById<ImageView>(R.id.imageView).setImageDrawable(drawable)
+    }
+
+    fun addGeometry(geometryView: LineGeometryView) {
+        var root = findViewById<ConstraintLayout>(R.id.root)
+        val params = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
+        geometryView.layoutParams = params
+        params.leftToLeft =  root.id
+        params.topToTop = root.id
+        params.bottomToBottom = root.id
+        params.rightToRight = root.id
+
+        disableGeometryViews(root)
+
+        root.addView(geometryView)
+    }
+
+    private fun disableGeometryViews(root: ConstraintLayout) {
+        for (child in root.children) {
+            if (child is LineGeometryView) {
+                child.isEnabled = false
+            }
+        }
     }
 }
--- app/src/main/java/com/devtechdesign/imageannotation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/devtechdesign/imageannotation/ui/home/HomeFragment.kt
@@ -4,28 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.devtechdesign.imageannotation.R
+import com.devtechdesign.imageannotation.ui.AnnotatedImageView
+import com.devtechdesign.imageannotation.ui.LineGeometryView
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
+    private var root: View? = null
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+         this.root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root?.findViewById(R.id.text_home)!!
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+
+        val btnAdd: Button = root?.findViewById(R.id.btnAdd)!!
+        btnAdd.setOnClickListener {
+            onAddButtonClick()
+        }
         return root
+    }
+
+    private fun onAddButtonClick() {
+        Toast.makeText(context, R.string.click_image_to_begin_adding_geometry, Toast.LENGTH_SHORT)
+            .show()
+
+        var lineGeometryView = LineGeometryView(requireContext())
+        val annotatedImageView: AnnotatedImageView? = root?.findViewById(R.id.annotatedImageView)
+        annotatedImageView?.addGeometry(lineGeometryView)
     }
 }
